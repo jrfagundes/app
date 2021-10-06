1eadcf9a2695fbe8de3c120cf97976c13bc353e2
Your Name <you@example.com>
2021-10-06 02:04:51 -0300

    Finalizando projeto

--- src/main/java/com/wine/app/controllers/LojaController.java
+++ src/main/java/com/wine/app/controllers/LojaController.java
@@ -10,7 +10,7 @@ import com.wine.app.models.repository.LojaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 
 
@@ -41,22 +39,47 @@ public class LojaController {
     }
 
     @PostMapping
-    public ResponseEntity<Loja> adicionarLoja(@RequestBody Loja loja) {        
-        return ResponseEntity.status(HttpStatus.CREATED).body(lojaRepository.save(loja));               
+    public ResponseEntity<Loja> adicionarLoja(@RequestBody Loja loja) {  
+        if(lojaRepository.ExistCepInRange(loja.getFaixaInicio(), loja.getFaixaFim())!=0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }     
+        if(loja.isValid()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(lojaRepository.save(loja));
+        } 
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);  
+        }             
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Loja> atualizarLoja(@PathVariable Long id, @RequestBody Loja loja) {
+        Long faixaInicio = 0L;
+        Long faixaFim = 0L;
+        if(loja.getFaixaInicio()!=null) faixaInicio=loja.getFaixaInicio(); 
+        if(loja.getFaixaFim()!=null) faixaFim=loja.getFaixaFim();        
+       
+        if(lojaRepository.ExistCepInRange(faixaInicio, faixaFim)!=0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }  
         Loja lojaAtual = lojaRepository.findById(id).get();
         if (lojaAtual != null) {
             if (loja.getCodigoLoja() != null) lojaAtual.setCodigoLoja(loja.getCodigoLoja());
             if (loja.getFaixaInicio() != null) lojaAtual.setFaixaInicio(loja.getFaixaInicio());
             if (loja.getFaixaFim() != null) lojaAtual.setFaixaFim(loja.getFaixaFim());
-
+            if (!lojaAtual.isValid()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            } 
             lojaRepository.save(lojaAtual);
             return ResponseEntity.ok(lojaAtual);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> DeletarCep(@PathVariable Long id){
+        lojaRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+        
     }
     
 
--- src/main/java/com/wine/app/models/repository/LojaRepository.java
+++ src/main/java/com/wine/app/models/repository/LojaRepository.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 import com.wine.app.models.Loja;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LojaRepository extends JpaRepository<Loja, Long>{
     Optional<Loja> findById(Long id);
+
+    @Query("SELECT COUNT(l) AS total FROM Loja l WHERE (?1 BETWEEN faixa_inicio AND faixa_fim) OR (?2 BETWEEN faixa_inicio AND faixa_fim)")
+    int ExistCepInRange(Long faixaInicio, Long faixaFim);
 	
 }
